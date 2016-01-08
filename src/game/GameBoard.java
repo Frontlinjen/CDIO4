ab30eddeb56806fcbfd9e8316ada22ce987556f2
Hjorthen <Sebastian@10.16.96.34>
2016-01-08 12:10:32 +0100

    Updated shufflebag references

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -1,6 +1,7 @@
 package game;
 
 import slots.*;
+import utilities.ShuffleBag;
 import desktop_resources.GUI;
 
 public class GameBoard {
@@ -11,29 +12,19 @@ public class GameBoard {
 		//desktop_fields.Brewery b = new desktop_fields.Brewery.Builder().setRent("2000").build();
 		
 		System.out.println("Loading board...");
-		Field[] tmpFields = FieldLoader.parseFields("Fields.xml");
-		for(Field f : tmpFields)
+		fields = FieldLoader.parseFields("Fields.xml");
+		for(Field f : fields)
 		{
 			System.out.println(f.getName());
 		}
-		ShuffleBag<Field> shuffle = new ShuffleBag<Field>(tmpFields);
-		desktop_fields.Field[] guiFields = new desktop_fields.Field[tmpFields.length];
-		fields = new Field[tmpFields.length];
+		desktop_fields.Field[] guiFields = new desktop_fields.Field[fields.length];
 		
 		int pos = 1;
-		while(shuffle.elementsLeft()>0)
+		for(Field f : fields)
 		{
-			Field f;
-			try {
-				f = shuffle.getNext();
-				desktop_fields.Field guiField = f.pushToGUI(pos);
-				guiFields[pos-1] = guiField;
-				fields[pos-1] = f;
-				++pos;
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			desktop_fields.Field guiField = f.pushToGUI(pos++);
+			guiFields[pos-1] = guiField;
+			
 		}
 		GUI.create(guiFields);
 	}
