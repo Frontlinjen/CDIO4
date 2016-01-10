af849568a1effb3c21d2737a1bddcfbc1e3e8309
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-10 17:46:27 +0100

    ChanceCard class
    Added variables and methods for the ChanceCard class

--- src/Chancecards/ChanceCard.java
+++ src/Chancecards/ChanceCard.java
@@ -1,5 +1,20 @@
 package Chancecards;
 
-public class ChanceCard {
+import game.Player;
 
+public abstract class ChanceCard {
+	private String description;
+	private Player player;
+	
+	public abstract void onDrawn(Player player);
+	
+	public String getDescription() 
+	{
+		return description;
+	}
+	
+	public void chanceCard(String description)
+	{
+		
+	}
 }
