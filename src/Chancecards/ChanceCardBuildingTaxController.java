8c0f3cc7face982138fb8695b69b9c56ca7e3def
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-11 17:25:35 +0100

    BuildingTaxController
    Made the onDrawn method. Return true if the player has houses or hotels,
    return false if not.

--- src/Chancecards/ChanceCardBuildingTaxController.java
+++ src/Chancecards/ChanceCardBuildingTaxController.java
@@ -1,6 +1,7 @@
 package Chancecards;
 
 import game.Player;
+import game.Account;
 
 public class ChanceCardBuildingTaxController extends ChanceCardController{
 	private ChanceCardBuildingTaxData chanceCardData;
@@ -11,8 +12,15 @@ public class ChanceCardBuildingTaxController extends ChanceCardController{
 
 	@Override
 	public boolean onDrawn(Player player) {
-		if(player.getProperty().getPropertiesOwned().) {
-			
+		int hu = player.getProperty().getTotalHouseCount();
+		int ho = player.getProperty().getTotalHotelCount();
+		Account acc = chanceCardData.getParkinglotAccount();
+		int housecount = chanceCardData.getHouseTax();
+		int hotelcount = chanceCardData.getHotelTax();
+		
+		if(hu+ho > 0) {
+			player.getAccount().transferTo(acc, housecount*hu);
+			player.getAccount().transferTo(acc, hotelcount*ho);
 			return false;
 		} else {
 			return true;
