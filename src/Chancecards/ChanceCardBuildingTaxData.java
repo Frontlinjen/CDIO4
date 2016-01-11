b7fa826d65088914d6fd4befb52a02405c9b75de
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-11 17:24:36 +0100

    BuildingTaxData
    Added getters for the variables for the controller to use.

--- src/Chancecards/ChanceCardBuildingTaxData.java
+++ src/Chancecards/ChanceCardBuildingTaxData.java
@@ -1,12 +1,28 @@
 package Chancecards;
 
+import game.Account;
+
 public class ChanceCardBuildingTaxData extends ChanceCardData{
 	private int houseTax;
 	private int hotelTax;
+	private Account parkinglotAccount;
 	
-	public ChanceCardBuildingTaxData(int translateID, int houseTax, int hotelTax) {
+	public ChanceCardBuildingTaxData(int translateID, int houseTax, int hotelTax, Account parkinglotAccount) {
 		super(translateID);
 		this.houseTax = houseTax;
 		this.hotelTax = hotelTax;
+		this.parkinglotAccount = parkinglotAccount;
+	}
+	
+	public int getHouseTax() {
+		return houseTax;
+	}
+	
+	public int getHotelTax() {
+		return hotelTax;
+	}
+	
+	public Account getParkinglotAccount(){
+		return parkinglotAccount;
 	}
 }
