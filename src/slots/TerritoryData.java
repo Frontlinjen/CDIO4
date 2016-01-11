836e7d05c6affde2dba618134e606974cebda6d1
Thomas Bech Madsen <thomasbm@live.dk>
2016-01-11 12:31:52 +0100

    Added buyHouse method to TerritoryController
    Added houses and houseCost variables to TerritoryData along with
    get-methods and add-methods

--- src/slots/TerritoryData.java
+++ src/slots/TerritoryData.java
@@ -10,15 +10,31 @@ import game.Translator;
 public class TerritoryData extends OwnableData{
 
 	private int rent;
-
-	public TerritoryData(int i, int price, int rent) {
+	private int houses;
+	private int houseCost;
+	
+	public TerritoryData(int i, int price, int rent, int houseCost) {
 		super(i, price);
 		this.rent = rent;
+		this.houseCost = houseCost;
+		houses = 0;
 	}
 
 
 	public int getRent() {
 		return rent;
+	}
+	
+	public int getHouses(){
+		return houses;
+	}
+	
+	public void addHouse(){
+		houses++;
+	}
+	
+	public int getHouseCost(){
+		return houseCost;
 	}
 
 }
