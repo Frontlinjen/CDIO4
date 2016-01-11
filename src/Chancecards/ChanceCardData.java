0c82adc5a73595b68ed01cc08a347d2500b58a30
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-11 13:26:38 +0100

    ChanceCard
    Splitted it up into a controller and a data

---
+++ src/Chancecards/ChanceCardData.java
@@ -0,0 +1,13 @@
+package Chancecards;
+
+import game.Player;
+import game.Translator;
+
+public abstract class ChanceCardData {
+	protected int translateID;
+	
+	public ChanceCardData(int translateID) {
+		this.translateID = translateID;
+	}
+	
+}
