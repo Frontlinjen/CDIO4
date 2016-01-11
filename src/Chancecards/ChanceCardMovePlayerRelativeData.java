69c46b030b653baf2e56938e74d9e56872f98626
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-11 13:24:58 +0100

    ChanceCardMovePlayerRelative
    Splitted it up to a controller and a data

---
+++ src/Chancecards/ChanceCardMovePlayerRelativeData.java
@@ -0,0 +1,22 @@
+package Chancecards;
+
+import game.Player;
+
+public class ChanceCardMovePlayerRelativeData extends ChanceCardData{
+	private int distance;
+	private boolean cashAtStart;
+	
+	public ChanceCardMovePlayerRelativeData(int translateID, int distance, boolean cashAtStart) {
+		super(translateID);
+		this.distance = distance;
+		this.cashAtStart = cashAtStart;
+	}
+	
+	public int getDistance() {
+		return distance;
+	}
+	
+	public boolean getCashAtStart() {
+		return cashAtStart;
+	}
+}
