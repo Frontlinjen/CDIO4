a773dd7adaeec790889419329132bc7ab9a572f9
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-11 13:20:50 +0100

    ChanceCardBuildingTax
    Splitted the class up in a controller class and a data class.

---
+++ src/Chancecards/ChanceCardBuildingTaxController.java
@@ -0,0 +1,23 @@
+package Chancecards;
+
+import game.Player;
+
+public class ChanceCardBuildingTaxController extends ChanceCardController{
+	private ChanceCardBuildingTaxData chanceCardData;
+	
+	public ChanceCardBuildingTaxController(ChanceCardBuildingTaxData chanceCardData) {
+		this.chanceCardData = chanceCardData;
+	}
+
+	@Override
+	public void onDrawn(Player player) {
+		
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
