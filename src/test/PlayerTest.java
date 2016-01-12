1661326faf8d3792cf6b26d4d5ef383c4cd26716
SenadBegovic <senadbegovic2500@hotmail.com>
2016-01-12 14:19:59 +0100

    Finished all tests in PlayerTest class

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -1,35 +1,65 @@
 package test;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import game.Player;
 
 public class PlayerTest {
-
+	
+	Player player;
+	
+	@Before
+	public void initialize(){
+	player = new Player("Sheep");
+	}
+	
 	@Test
 	public void testGetAccount() {
-		Player player = new Player("Sheep");
 		assertTrue("Fail, the player should have 30000 gold to start with.", player.getAccount().getGold()==30000);
 	}
 
 	@Test
+	public void testGetName(){
+		assertTrue("Fail, the player should have the name \"Sheep\"", player.getName().equals("Sheep"));
+	}
+	
+	@Test
 	public void testGetProperty() {
-		Player player = new Player("Sheep");
 		assertTrue("Fail, A player should not own anything at the start of the game.", player.getProperty().getPropertyCount() == 0);
 	}
 
 	@Test
 	public void testMove() {
-		Player player = new Player("Sheep");
 		player.move(22, true);
-		assertTrue("Fail, the player should be on the first",player.getPosition() == 0);
+		assertTrue("Fail, the player should be on the first",player.getPosition() == 0 && player.getNextPosition() == 22);
 	}
 
 	@Test
-	public void testGetPosition() {
-		Player player = new Player("Sheep");
-		assertTrue("Fail, the player should not have a position without having had a turn", player.getPosition()==0);
+	public void testMoveToNextPosition(){
+		player.move(22, true);
+		player.move(10, true);
+		player.moveToNextPosition();
+		assertTrue("Fail, the player should be on position 22", player.getPosition() == 32);
+	}
+	
+	@Test
+	public void testSetNextPosition(){
+		player.setNextPosition(15, true);
+		player.moveToNextPosition();
+		assertTrue("Fail, the player should be on position 15", player.getPosition() == 15);
+	}
+	
+	@Test
+	public void testHasGetOutOfPrisonCard(){
+		assertTrue("Fail, the player should not start with the getOutOfPrisonCard", !player.hasGetOutOfPrisonCard());
+	}
+	
+	@Test
+	public void testSetGetOutOfPrisonCard(){
+		player.setHasGetOutOfPrisonCard(true);
+		assertTrue("Fail, the player should have the getOutOfPrisonCard", player.hasGetOutOfPrisonCard());
 	}
 
 }
