2101f98dc81bbe718b36d406c7a60d6d95f51aeb
SenadBegovic <senadbegovic2500@hotmail.com>
2016-01-13 08:55:44 +0100

    Fixed some test fixes in FleetControllerTest class

--- src/test/FleetControllerTest.java
+++ src/test/FleetControllerTest.java
@@ -15,13 +15,13 @@ public class FleetControllerTest {
 		Player player1 = new Player("Sheep");
 				
 		ship1.setOwner(player1);
-		player1.getProperty().addProperty(ship1);
+		player1.getProperty().addFleet(ship1);
 		assertTrue("Fail, the rent should be 500 with 1 fleet owned",ship1.getRent()==500);
-		player1.getProperty().addProperty(ship1);
+		player1.getProperty().addFleet(ship1);
 		assertTrue("Fail, the rent should be 1000 with 2 fleet owned",ship1.getRent()==1000);
-		player1.getProperty().addProperty(ship1);
-		assertTrue("Fail, the rent should be 1500 with 3 fleet owned",ship1.getRent()==1500);
-		player1.getProperty().addProperty(ship1);
-		assertTrue("Fail, the rent should be 2000 with 4 fleet owned",ship1.getRent()==2000);
+		player1.getProperty().addFleet(ship1);
+		assertTrue("Fail, the rent should be 1500 with 3 fleet owned",ship1.getRent()==2000);
+		player1.getProperty().addFleet(ship1);
+		assertTrue("Fail, the rent should be 2000 with 4 fleet owned",ship1.getRent()==4000);
 	}
 }
