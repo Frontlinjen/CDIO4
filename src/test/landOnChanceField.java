7c90f039de65a37a3682281e92e234041c0e033c
Hjorthen <Sebastian@SebastianPC>
2016-01-14 19:49:19 +0100

    Added landOnChanceField test

--- src/test/landOnChanceField.java
+++ src/test/landOnChanceField.java
@@ -8,11 +8,11 @@ import game.Board;
 import game.DiceCup;
 import game.TestDice;
 
-public class GotoPrisonTest {
+public class landOnChanceField {
 
 	@Test
 	public void test() {
-		TestDice dice = new TestDice(new int[][]{{30,0},{30,0},{20,0},{20,0},{20,0},{20,0}});
+		TestDice dice = new TestDice(new int[][]{{2,0},{2,0},{5,0},{5,0},{10,0},{10,0}});
 		Board board = new Board((DiceCup)dice);
 		board.startGame();
 	}
