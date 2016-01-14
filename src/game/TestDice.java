042500b35289866b1f536c4cedfb162a9f63c4c6
Hjorthen <Sebastian@SebastianPC>
2016-01-14 15:39:37 +0100

    Created a testDice which outputs results in a pre-defined sequence

---
+++ src/game/TestDice.java
@@ -0,0 +1,25 @@
+package game;
+
+public class TestDice extends DiceCup {
+	int[][] diceSequence;
+	//Has to be -1 since it gets increased by one before its used for the first time
+	int currentIndex = -1;
+	
+	public TestDice(int[][] diceSequence)
+	{
+		super(2);
+		this.diceSequence = diceSequence;
+	}
+	@Override 
+	public DiceResult rollDice(){
+		if(currentIndex+1==diceSequence.length)
+		{
+			currentIndex = 0;
+		}
+		else
+		{
+			++currentIndex;
+		}
+		return new DiceResult(diceSequence[currentIndex]);
+	}
+}
