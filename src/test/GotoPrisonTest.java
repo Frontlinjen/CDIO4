533141fc9fec50f33c13de1fc5263dbf531158c3
Hjorthen <Sebastian@SebastianPC>
2016-01-14 19:24:15 +0100

    Added a goto prison test

---
+++ src/test/GotoPrisonTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import game.Board;
+import game.DiceCup;
+import game.GameBoard;
+import game.TestDice;
+
+public class GotoPrisonTest {
+
+	@Test
+	public void test() {
+		TestDice dice = new TestDice(new int[][]{{30,0},{30,0},{20,0},{20,0},{20,0},{20,0}});
+		Board board = new Board((DiceCup)dice);
+		board.startGame();
+	}
+
+}
