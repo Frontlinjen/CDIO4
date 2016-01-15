6457c5af635648855e46c75435b1666d52773902
MikkelLeth <MikkelLeth@10.16.175.95>
2016-01-15 09:40:25 +0100

    BreweryData
    Added a variable called pawnvalue and added it to the constructor

--- src/slots/BreweryData.java
+++ src/slots/BreweryData.java
@@ -3,10 +3,16 @@ package slots;
 public class BreweryData extends OwnableData{
 	
 	private int baserent;
+	private int pawnvalue;
 	
-	public BreweryData(int rent, int translateID, int price) {
+	public BreweryData(int rent, int translateID, int price, int pawnvalue) {
 		super(translateID, price);
 		this.baserent = rent;
+		this.pawnvalue = pawnvalue;
+	}
+	
+	public int getPawnValue(){
+		return pawnvalue;
 	}
 	
 	public String toString(){
